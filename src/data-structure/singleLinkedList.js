b4c96341e9716aa1757b6e1f25ebf34ea37f9c08
zerozoo <zerozoo385@gmail.com>
2022-12-07 02:14:48 +0900

    [CREATE]: simple js linked list 추가

--- src/data-structure/singleLinkedList.js
+++ src/data-structure/singleLinkedList.js
@@ -1,88 +1,45 @@
 class Node {
   constructor(val) {
-    this.value = val;
+    this.val = val;
     this.next = null;
   }
 }
-
-class SingleLinkedList {
+class LinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
-    this.size = 0;
+    this.length = null;
   }
-  push(val) {
-    const newNode = new Node(val);
-    if (!this.head) {
-      this.head = newNode;
-      this.tail = newNode;
+
+  add(val) {
+    const node = new Node(val);
+    let current = this.head;
+    if (this.head === null) {
+      this.head = node;
     } else {
-      this.tail.next = newNode;
-      this.tail = newNode;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = node;
     }
-    this.size++;
+    this.tail = node;
+    this.length++;
     return this;
   }
 
-  pop() {
-    /** case: empty list */
-    if (!this.head) return;
-
-    /** case: one node */
-    if (this.length === 1) {
-      this.head = null;
-      this.tail = null;
-      this.length = 0;
-      return;
+  traverse1(node, n = 0) {
+    if (node !== null) {
+      this.traverse1(node.next, n + 1);
+      console.log(`data: ${n}`, node.val);
     }
-
-    /** case: many nodes */
-    let current = this.head;
-    let newTail = null;
-
-    while (current.next) {
-      current = current.next;
-      /** current.next가 없을 경우 newTail이 마지막 list를 저장함 */
-      newTail = current;
-    }
-    this.tail = newTail;
-    this.tail.next = null;
-    this.size--;
-    if (this.size === 0) {
-      this.head = null;
-      this.tail = null;
-    }
-    return current;
-  }
-
-  get(index) {
-    if (index < 0 || index >= this.size) {
-      return;
-    }
-    let current = this.head;
-    for (let i = 0; i !== index; i++) {
-      current = current.next;
-    }
-    return current;
-  }
-
-  map(f) {
-    const result = [];
-    for (let p = this.head; p !== null; p = p.next) {
-      result.push(f(p.value));
-    }
-    return result;
   }
 }
 
-const list = new SingleLinkedList();
-list.push(0);
-list.push(1);
-list.push(2);
-list.push(3);
-console.log(
-  "list >>",
-  list.map((a) => a + 1)
-);
+const conga = new LinkedList();
 
-console.log(" list 2 ", list.get(2));
+conga.add("a");
+conga.add("b");
+conga.add("c");
+conga.add("d");
+console.log("conga", conga);
+conga.traverse1(conga.head);
