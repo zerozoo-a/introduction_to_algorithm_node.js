1fbbc1639937e2d5a818d50e1041293db0be701d
zerozoo <zerozoo385@gmail.com>
2022-12-25 22:48:04 +0900

    [CREATE]: 문자열의 길이를 재귀로 구하기

--- src/problems/dp/1/5.minCost.js
+++ src/problems/dp/1/5.minCost.js
@@ -1,5 +1,4 @@
-const INT = 4;
-const cost = [
+const COST = [
   [0, 10, 75, 94],
   [-1, 0, 35, 50],
   [-1, -1, 0, 80],
@@ -12,9 +11,9 @@ const cost = [
  * @param {number} d
  */
 const minCost = (s, d) => {
-  if (s === d || s === d - 1) return cost[s][d];
+  if (s === d || s === d - 1) return COST[s][d];
 
-  let minValue = cost[s][d];
+  let minValue = COST[s][d];
   for (let i = s + 1; i < d; i++) {
     const temp = minCost(s, i) + minCost(i, d);
 
---
+++ src/problems/recursions/countLength.js
@@ -0,0 +1,15 @@
+/**
+ *
+ * @param {string} str
+ * @returns
+ */
+const countLength = (str) => {
+  if (str === "") return 0;
+
+  return 1 + countLength(str.substring(1));
+};
+
+const main = (() => {
+  const len = countLength("abc");
+  console.log("🚀 ~ file: countLength.js:14 ~ main ~ len", len);
+})();
